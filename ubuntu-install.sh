60cc2c6099b1f0462ea8588a52b2ee75df2455b5
17akash <akash.singh@antiersolutions.com>
2022-06-08 17:05:17 +0530

    some chanegs

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -18,20 +18,20 @@ cd $URS
                 git clone https://github.com/PEER-Inc/Mainnet-binary.git
         fi 
 
-        if [ ! -d $URS/.peer ]
+        if [ ! -d $URS/.peer_mainnet ]
             then
-                mkdir $URS/.peer
+                mkdir $URS/.peer_mainnet
                 cd $URS/Mainnet-binary
-                cp  customSpecRaw.json $URS/.peer/customSpecRaw.json
+                cp  customSpecRaw.json $URS/.peer_mainnet/customSpecRaw.json
                 chmod +x peer
                 cp peer /usr/bin/
             else
                 cd Mainnet-binary 
 		echo $PWD
-                    if [ ! -f $URS/.peer/customSpecRaw.json ]
+                    if [ ! -f $URS/.peer_mainnet/customSpecRaw.json ]
                         then
                             cd $URS/Mainnet-binary
-                            mv  customSpecRaw.json $URS/.peer/customSpecRaw.json
+                            mv  customSpecRaw.json $URS/.peer_mainnet/customSpecRaw.json
                             chmod +x peer
                             cp peer /usr/bin/
                     fi      
@@ -41,6 +41,9 @@ cd $URS
 peercmd(){
     if [ -x /usr/bin/peer ]
        	        then
+       	        cd $URS/peer-node-installer
+                chmod +x peer
+                cp peer /usr/bin/
                 echo "peer is ready to run"
             else
                 cd $URS/peer-node-installer
@@ -60,24 +63,33 @@ peercmd(){
 }
 
     peerconnect(){
-        echo "Enter Your Node Name: "
-        read x
+        while [ -z $x ]
+            do
+                echo "Enter Your Node Name: "
+                read x
+
+            if [ -z "$x" ]
+            then
+                 echo "Please enter your node name it can't be null"
+            fi
+            done
+
         num=$(( $RANDOM % 5 + 1 ))
 case $num in
 	1)
-	peer --base-path /data/"${x}" --chain $URS/.peer/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/52.202.60.27/tcp/30333/p2p/12D3KooWJSTcGcgqnWnkPHgp734ckfKaHJ1R8eidCv2MJdqjkUnz	
+	peer --base-path /data/"${x}" --chain $URS/.peer_mainnet/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/52.202.60.27/tcp/30333/p2p/12D3KooWJSTcGcgqnWnkPHgp734ckfKaHJ1R8eidCv2MJdqjkUnz	
 		;;
 	2)
-	peer --base-path /data/"${x}" --chain $URS/.peer/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/54.151.75.198/tcp/30333/p2p/12D3KooWT2YpBQpowttM27qn1rDNqfFxZse18hQrE4PF97nFah6C
+	peer --base-path /data/"${x}" --chain $URS/.peer_mainnet/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/54.151.75.198/tcp/30333/p2p/12D3KooWT2YpBQpowttM27qn1rDNqfFxZse18hQrE4PF97nFah6C
 		;;
 	3)
-	peer --base-path /data/"${x}" --chain $URS/.peer/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe  --name "${x}" --bootnodes /ip4/13.71.104.102/tcp/30333/p2p/12D3KooWP7Vp8KCDQd55Jk86fagtSp58cBZbnk8BNrH4fmi2Qaj4
+	peer --base-path /data/"${x}" --chain $URS/.peer_mainnet/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe  --name "${x}" --bootnodes /ip4/13.71.104.102/tcp/30333/p2p/12D3KooWP7Vp8KCDQd55Jk86fagtSp58cBZbnk8BNrH4fmi2Qaj4
 		;;
 	4)
-	peer --base-path /data/"${x}" --chain $URS/.peer/customSpecRaw.json  --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/52.231.97.26/tcp/30333/p2p/12D3KooWLrv3tMjUza2rjrhybUuUJTkyY549RzNgqx6tVzDQnRZr
+	peer --base-path /data/"${x}" --chain $URS/.peer_mainnet/customSpecRaw.json  --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/20.98.202.26/tcp/30333/p2p/12D3KooWEVSQiY9WNzTzXjgx2SAA8XjsFt4TCyfLNxKcTsuL4wFD
 		;;
 	5)
-	peer --base-path /data/"${x}" --chain $URS/.peer/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/34.89.242.29/tcp/30333/p2p/12D3KooWAS3YCVXWkUXR2EbogeW8qo7DAbMtYPJdRGbGCeeMyVtM
+	peer --base-path /data/"${x}" --chain $URS/.peer_mainnet/customSpecRaw.json --port 30333 --ws-external --rpc-external --rpc-cors all --no-telemetry --validator --rpc-methods Unsafe --name "${x}" --bootnodes /ip4/34.89.242.29/tcp/30333/p2p/12D3KooWAS3YCVXWkUXR2EbogeW8qo7DAbMtYPJdRGbGCeeMyVtM
 		;;
 esac
 }
